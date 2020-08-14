fe65e0af929ac1b548419228f596fa74c04c3558
cryptopinions3 <cryptopinions2@mail.com>
2020-08-14 16:10:56 -0600

    switch to mainnet

--- interface.js
+++ interface.js
@@ -1,13 +1,13 @@
 
 //mainnet
 //tokenContractAddress=""
-//airdropContractAddress=""
+airdropContractAddress="0xeEBD4b9Cc99c361212e4b3Ad63c49aCE99991664"
 
 //testnet
-//0xEfA85A029fcaA1Ec462019998A7B537b686BfF34
-//tokenContractAddress="0xc258c7e80E9e441f69fc3d25582c0B577b8df0ba"
-airdropContractAddress="0x69b0e7dE2819307CeB366B4032547Abea0df499A"//"0x0DC282E46Bf1A7b3Eec97ba77b05F308C68D851e"//"0xacd98510386cA008133D8Ac230990D4c15F7edc6"
-//"0x297FBB0688945d15Be3Eed8df08D34b8780D5a63"//
+// //0xEfA85A029fcaA1Ec462019998A7B537b686BfF34
+// //tokenContractAddress="0xc258c7e80E9e441f69fc3d25582c0B577b8df0ba"
+// airdropContractAddress="0x69b0e7dE2819307CeB366B4032547Abea0df499A"//"0x0DC282E46Bf1A7b3Eec97ba77b05F308C68D851e"//"0xacd98510386cA008133D8Ac230990D4c15F7edc6"
+// //"0x297FBB0688945d15Be3Eed8df08D34b8780D5a63"//
 setup()
 function setup(){
   window.addEventListener('load', async () => {
@@ -34,8 +34,8 @@ function setup(){
     // Non-dapp browsers...
     else {
       alert('please ensure https://metamask.io/ is installed and connected ')
-      web3 = new Web3('wss://ropsten.infura.io/ws');
-      //web3 = new Web3('wss://mainnet.infura.io/ws');
+      //web3 = new Web3('wss://ropsten.infura.io/ws');
+      web3 = new Web3('wss://mainnet.infura.io/ws');
       console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
     web3.eth.net.getId().then(function(nid){
@@ -45,10 +45,10 @@ function setup(){
 		console.log('should be checking network')
     web3.eth.net.getNetworkType().then(function(ntype){
       console.log('network ',ntype)
-      //if(ntype!='main'){
-      if(ntype!='rinkeby'){
-        alert('please switch to rinkeby in Metamask')
-        //alert('please switch to mainnet in Metamask')
+      if(ntype!='main'){
+      //if(ntype!='rinkeby'){
+        //alert('please switch to rinkeby in Metamask')
+        alert('please switch to mainnet in Metamask')
       }
     })
 		//window.tokenContract=new web3.eth.Contract(tokenAbi,tokenContractAddress)
